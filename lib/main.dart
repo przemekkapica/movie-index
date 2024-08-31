0773bffdc468247820f8654fd9acaf14e6bd1928
przemekkapica <przemekapica@gmail.com>
2024-08-31 19:38:04 +0200

    feat: configure DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:movie_index/app/app.dart';
+import 'package:movie_index/core/di/di_config.dart';
 import 'package:movie_index/presentation/routing/app_router.dart';
 
 void main() {
+  configureDependencies();
+
   runApp(
     App(appRouter: AppRouter()),
   );
